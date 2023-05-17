28eaafab7d0edb0ebf1f8a51581084c6496fdb88
AlexeiKonev <alexkonev777@gmail.com>
2023-05-17 11:53:39 +0300

    add model Comment

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title # возвращает заголовок поста при выводе на экран
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) # пост, к которому относится комментарий
+    name = models.CharField(max_length=50) # имя комментатора
+    email = models.EmailField() # электронная почта комментатора
+    content = models.TextField() # содержание комментария
+    date_created = models.DateTimeField(auto_now_add=True) # дата создания комментария
+
+    def __str__(self):
+        return f"{self.name} on {self.post}" # возвращает имя комментатора и пост при выводе на экран
